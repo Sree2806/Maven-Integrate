3bbde6409e87b96bbcdb4f285bf5eae992942583
Sree2806 <sreelakshmisuresh061@gmail.com>
2021-07-07 12:10:39 +0530

    Sree's first commit

---
+++ src/main/java/tests/first.java
@@ -0,0 +1,16 @@
+package tests;
+
+
+import org.testng.annotations.Test;
+
+public class first {
+
+		
+	  @Test
+	  public void Test2() {
+		System.out.println("First after New");
+	  }
+	  
+	 
+	
+}
---
+++ src/main/java/tests/second.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class second {
+
+	@Test
+	  public void Test3() {
+		System.out.println("Second after New");
+	  }
+}
